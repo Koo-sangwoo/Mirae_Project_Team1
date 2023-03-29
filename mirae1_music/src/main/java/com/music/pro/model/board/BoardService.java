99defb402520e2d7582d57ddac4c2f22b024af48
LG <LG@DESKTOP-RFKBN5P>
2023-03-29 15:57:23 +0900

    [오류로다시커밋]

--- mirae1_music/src/main/java/com/music/pro/model/board/BoardService.java
+++ mirae1_music/src/main/java/com/music/pro/model/board/BoardService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.music.pro.model.board;
 
 import java.util.List;
@@ -20,26 +19,3 @@ public interface BoardService {
     void replyCnt(int board_id) throws Exception;  //게시글 댓글수
     public int getTotal(Criteria cri) throws Exception; // 총게시글 수
 }
-=======
-package com.music.pro.model.board;
-
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
-
-import com.music.pro.controller.board.Criteria;
-import com.music.pro.vo.board.BoardVO;
-
-public interface BoardService {
-	
-	List<BoardVO> listAllBoard() throws Exception;	// 게시글 목록
-	public List<BoardVO> listAllBoardPaging(Criteria cri); /* 게시판 목록(페이징 적용) */
-	void createBoard(BoardVO vo) throws Exception;	// 게시글 쓰기 
-	public BoardVO readBoard(int board_id) throws Exception; //게시글 상세보기
-	void deleteBoard(int board_id) throws Exception;						// 게시글 삭제
-    void updateBoard(BoardVO vo) throws Exception;          //게시글 수정
-    void viewCnt(int board_id) throws Exception; 	//게시글 조회수
-    void replyCnt(int board_id) throws Exception;  //게시글 댓글수
-    public int getTotal() throws Exception; // 총게시글 수
-}
->>>>>>> branch 'main' of https://github.com/Koo-sangwoo/Mirae_Project_Team1.git
--- mirae1_music/src/main/java/com/music/pro/model/board/BoardServiceImpl.java
+++ mirae1_music/src/main/java/com/music/pro/model/board/BoardServiceImpl.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.music.pro.model.board;
 
 import java.util.List;
@@ -61,67 +60,3 @@ import com.music.pro.model.board.BoardService;
 			return boardDAO.getTotal(cri);
 		 }
 	}
-=======
-package com.music.pro.model.board;
-
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.music.pro.vo.board.BoardDAO;
-import com.music.pro.vo.board.BoardVO;
-import com.music.pro.controller.board.Criteria;
-import com.music.pro.model.board.BoardService;
-
-	@Service("BoardService")
-	public class BoardServiceImpl implements BoardService {
-		@Autowired
-		private BoardDAO boardDAO;
-
-		// 게시글 목록
-		public List<BoardVO> listAllBoard() throws Exception {
-			return boardDAO.listAllBoard();
-		}
-		
-		 /* 게시판 목록(페이징 적용) */
-	   
-	    public List<BoardVO> listAllBoardPaging(Criteria cri) {
-	        return boardDAO.listAllBoardPaging(cri);
-	    }    
-		
-		// 게시글 쓰기
-		public void createBoard(BoardVO vo) throws Exception {
-			boardDAO.createBoard(vo);
-		}
-		
-		// 게시글 읽기
-		public BoardVO readBoard(int board_id) throws Exception {
-			return boardDAO.readBoard(board_id);
-		}
-		
-		// 게시글 수정
-		public void updateBoard(BoardVO vo) throws Exception {
-			boardDAO.updateBoard(vo);
-		}
-		
-		// 게시글 삭제
-		public void deleteBoard(int board_id) throws Exception {
-			boardDAO.deleteBoard(board_id);
-		}
-		// 게시글 조회수
-		public void viewCnt(int board_id) throws Exception {
-		boardDAO.viewCnt(board_id);
-        }
-		// 게시글 제목에 댓글수 표시
-		public void replyCnt(int board_id) throws Exception	{
-			boardDAO.replyCnt(board_id);
-	}
-		//총 게시글 수
-		 public int getTotal() throws Exception {
-			return boardDAO.getTotal();
-		 }
-	}
->>>>>>> branch 'main' of https://github.com/Koo-sangwoo/Mirae_Project_Team1.git
